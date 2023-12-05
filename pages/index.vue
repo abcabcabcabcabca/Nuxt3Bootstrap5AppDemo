2482634ec9105c786e250f0ad0d88e1d9cbeb77a
abcabcabcabcabca <144089393+abcabcabcabcabca@users.noreply.github.com>
2023-12-05 05:50:25 +0000

    12/5

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
+<template>
+<div class="container text-center my-5">
+<h1>TOYOTA</h1>
+</div>
+<div class="card mb-3 mx-auto">
+<div class="row g-0">
+<div class="col-md-4">
+<div class="col-sm">
+<div class="container mb-5">
+<div class="container text-center my-5">
+<h3>最新の車(セダン、センチュリー、ミニバン)</h3>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 <div class="container mb-5">
 <h1>セダン</h1>
+<div class="container mb-5">
 <div class="row gx-5">
 <div class="col-sm">
+</div>
+</div>
+</div>
+</div>
 <h2>CROWN</h2>
 <img
             src="https://toyota.jp/pages/contents/top/image/pr_area/crown/crown_pc_1.jpg"
@@ -10,82 +32,97 @@
 <div class="col border p-2">
 <div class="mb-2"><span class="badge bg-secondary">1</span></div>
 <p>FCEVという新たな選択肢を加え、
-ニューフォーマルに進化したクラウン。
-</p>
+ニューフォーマルに進化したクラウン。</p>
 </div>
-
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+      <div class="col border p-2">
+        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+<p>威厳と風格を備えた、美しい佇まい。
+室内にひろがるおもてなし空間。
+優雅で心地良い走り。</p>
+</div>
+</div>
+<div class="container mb-5"></div>
+<h1>センチュリー</h1>
 <div class="container mb-5">
+<div class="row gx-5">
+<div class="col-sm">
+</div>
+</div>
+</div>
 <h2>CENTURY</h2>
 <img
             src="https://toyota.jp/pages/contents/top/image/pr_area/centurynewbt/century_pc_1.jpg"
             class="img-fluid rounded-start" alt="...">
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4"></div>
-<div class="col border p-2"></div>
-<div class="mb-2"><span class="badge bg-secondary">1</span></div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+      <div class="col border p-2">
+        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
 <p>独自の美意識や思想を継承・進化。
-   後席空間に新しい価値を創造した、
-   ショーファーカーのもう一つの理想。
+後席空間に新しい価値を創造した、
+ショーファーカーのもう一つの理想。</p>
+</div>
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+      <div class="col border p-2">
+        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+<p>日本の美、匠の技、唯一無二の存在感
+より有意義な移動時間を提供する後席
+著しい進化を遂げた静けさと乗り心地
 </p>
 </div>
-<div class="col border p-2">
-<div class="mb-2"><span class="badge bg-secondary">2</span></div>
-<p>
-    日本の美、匠の技、唯一無二の存在感
-    より有意義な移動時間を提供する後席
-    著しい進化を遂げた静けさと乗り心地
-</p>
 </div>
- </div>
-
-</div>
-<div class="container mb-5">
+<div class="container mb-5"></div>
 <h1>ミニバン</h1>
+<div class="container mb-5">
 <div class="row gx-5">
 <div class="col-sm">
+</div>
+</div>
+</div>
 <h3>VELLFIRE</h3>
-  <img
+<img
             src="https://toyota.jp/pages/contents/top/image/pr_area/vellfire/vellfire_pc_1.jpg"
             class="img-fluid rounded-start" alt="...">
 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-<div class="col border p-2">
-<div class="mb-2"><span class="badge bg-secondary">1</span></div>
-<p>
-毎日の移動を、アグレシッブに。
+      <div class="col border p-2">
+        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+<p>毎日の移動を、アグレシッブに。
 圧倒的な力強さと極上の快適さに
 心が昂ぶる、大空間サルーン。
- </P>
- </div>
- <div class="col border p-2">
-<div class="mb-2"><span class="badge bg-secondary">2</span></div>
-<p>
-力強さと心地よさを両立した動的性能
+</P>
+</div>
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+      <div class="col border p-2">
+        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+<p>力強さと心地よさを両立した動的性能
 移動の快適さを極めた後席空間
 周囲を圧倒するエクステリアデザイン
 </p>
 </div>
-<div class="col-sm">
-<div class="container mb-5">
+</div>
+<div class="container mb-5"></div>
 <h3>ALPHARD</h3>
-  <img
+<img
             src="https://toyota.jp/pages/contents/top/image/pr_area/alphard/alphard_pc_1.jpg"
             class="img-fluid rounded-start" alt="...">
- <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
 <div class="col border p-2">
 <div class="mb-2"><span class="badge bg-secondary">1</span></div>
-<p>
-一歩先へ進化を遂げた究極のもてなし空間。
+<p>一歩先へ進化を遂げた究極のもてなし空間。
 隅々に行き渡る快適さが、
 あらゆる移動をくつろぎに変えていく。
- </p>
- </div>
-  <div class="col border p-2">
+</p>
+</div>
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+<div class="col border p-2">
 <div class="mb-2"><span class="badge bg-secondary">2</span></div>
-<p>
-おもてなし装備に満ちた後席空間
+<p>おもてなし装備に満ちた後席空間
 上質を磨き上げたエクステリアデザイン
 大切な人を守り抜く安全性能
 </p>
-</div>
 </div>
 </div>
 </template>
