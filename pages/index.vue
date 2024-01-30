1c7dce92b92153330129eab966057939e41aa21c
abcabcabcabcabca <144089393+abcabcabcabcabca@users.noreply.github.com>
2024-01-30 05:46:11 +0000

    1月３０

--- pages/index.vue
+++ pages/index.vue
@@ -1,110 +1,97 @@
 <template>
-<div class="container text-center my-5">
-<h1>世界と日本の車</h1>
-</div>
-<div class="card mb-3 mx-auto">
-<div class="row g-0">
-</div>
-</div>
-<div class="container mb-6"></div>
-<div class="col-md-6">
-<div class="row g-0">
-<h1>世界で一番値段が高い車</h1>
-<div class="container mb-6">
-<div class="row gx-5">
-<div class="col-sm">
-</div>
-</div>
-</div>
-<h2>メルセデスベンツ 300slr ウーレンハウトクーペ</h2>
-<img
-            src="https://preview.free3d.com/img/2018/11/2399374330313049397/jafz01by.jpg"
-            class="img-fluid rounded-start" alt="...">
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-<div class="col border p-2">
-<div class="mb-6"><span class="badge bg-secondary">1</span></div>
-<p>オークションで1億3500万ユーロ（約213億1000万円）という破格の値段で落札された</p>
-</div>
-</div>
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-6"><span class="badge bg-secondary">2</span></div>
-<p>"SLR"はドイツ語でSport, Leicht, Rennwagen（スポーツ・軽量・レーシングカー）をあらわす</p>
-</div>
-</div>
-<div class="container mb-6"></div>
-</div>
-</div>
-<h1>日本で一番値段が高い車</h1>
-<div class="container mb-6">
-<div class="row gx-5">
-<div class="col-sm">
-</div>
-</div>
-</div>
-<h2>レクサスLFA</h2>
-<img
-            src="https://img.amiami.jp/images/product/main/231/TOY-SCL3-53269.jpg"
-            class="img-fluid rounded-start" alt="...">
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
-<p>価格は3750万円（ニュルブルクリンクパッケージは約4500万円）</p>
-</div>
-</div>
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
-<p>最高速度は320km/h
-</p>
-</div>
-</div>
-<div class="container mb-5"></div>
-<h1>日本で一番売れた車（2023年）</h1>
-<div class="container mb-5">
-<div class="row gx-5">
-<div class="col-sm">
-</div>
-</div>
-</div>
-<h3>登録車で１位はトヨタ「ヤリス」</h3>
-<img
-            src="https://www.weins-toyota-kanagawa.co.jp/file/cms/13701/3124628/image.jpg"
-            class="img-fluid rounded-start" alt="...">
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
-<p>販売台数19万4364台
-</P>
-</div>
-</div>
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
-<p>2020年から4年連続でトップ
-</p>
-</div>
-</div>
-<div class="container mb-5"></div>
-<h3>軽四輪車で１位はホンダ「N-BOX」</h3>
-<img
-            src="https://tjniigata.jp/assets/uploads/2022/01/NBOX-760x503.jpg"
-            class="img-fluid rounded-start" alt="...">
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-<div class="col border p-2">
-<div class="mb-2"><span class="badge bg-secondary">1</span></div>
-<p>販売台数23万1385台
-</p>
-</div>
-</div>
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-<div class="col border p-2">
-<div class="mb-2"><span class="badge bg-secondary">2</span></div>
-<p>ホンダ「N-BOX」が唯一20万台超えの販売台数を記録して1位となった
-</p>
-</div>
-</div>
+  <div class="container text-center my-5">
+    <h1>世界と日本の車</h1>
+  </div>
+
+  <div class="card mb-3 mx-auto">
+    <div class="row g-0">
+      <div class="col-md-6">
+        <h1>世界で一番値段が高い車</h1>
+        <div class="container mb-6">
+          <div class="row gx-5">
+            <div class="col-sm">
+              <h2>メルセデスベンツ 300slr ウーレンハウトクーペ</h2>
+              <img src="https://preview.free3d.com/img/2018/11/2399374330313049397/jafz01by.jpg"
+                class="img-fluid rounded-start" alt="...">
+            </div>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-6"><span class="badge bg-secondary">1</span></div>
+            <p>オークションで1億3500万ユーロ（約213億1000万円）という破格の値段で落札された</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-6"><span class="badge bg-secondary">2</span></div>
+            <p>"SLR"はドイツ語でSport, Leicht, Rennwagen（スポーツ・軽量・レーシングカー）をあらわす</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <h1>日本で一番値段が高い車</h1>
+        <div class="container mb-6">
+          <div class="row gx-5">
+            <div class="col-sm">
+              <h2>レクサスLFA</h2>
+              <img src="https://img.amiami.jp/images/product/main/231/TOY-SCL3-53269.jpg"
+                class="img-fluid rounded-start" alt="...">
+            </div>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+            <p>価格は3750万円（ニュルブルクリンクパッケージは約4500万円）</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+            <p>最高速度は320km/h</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mb-5">
+    <h1>日本で一番売れた車（2023年）</h1>
+    <div class="row gx-5">
+      <div class="col-sm">
+        <h3>登録車で１位はトヨタ「ヤリス」</h3>
+        <img src="https://www.weins-toyota-kanagawa.co.jp/file/cms/13701/3124628/image.jpg"
+          class="img-fluid rounded-start" alt="...">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+            <p>販売台数19万4364台</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+            <p>2020年から4年連続でトップ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row gx-5">
+      <div class="col-sm">
+        <h3>軽四輪車で１位はホンダ「N-BOX」</h3>
+        <img src="https://tjniigata.jp/assets/uploads/2022/01/NBOX-760x503.jpg"
+          class="img-fluid rounded-start" alt="...">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+            <p>販売台数23万1385台</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+            <p>ホンダ「N-BOX」が唯一20万台超えの販売台数を記録して1位となった</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
 <script setup>
 const data = useRecipe()
 </script>
--- pages/recipe.vue
+++ pages/recipe.vue
@@ -1,96 +1,97 @@
 <template>
   <div class="container text-center my-5">
-    <h1>{{ data.title }}</h1>
-    <div class="alert alert-primary" role="alert">{{ data.description }}</div>
+    <h1>世界と日本の車</h1>
   </div>
-  <div class="container mb-5">
-    <div class="card mb-3 mx-auto">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img
-            src="https://img.cpcdn.com/recipes/3695776/894x1461s/03855e67f64548a05b1f995faabe8f57?u=12868943&p=1455726325"
-            class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">{{data.zairyo}}</h5>
-            <div class="card-text">
-              <ul>
-                <li v-for="item in data.material">{{ item.text }}</li>
-              </ul>
+
+  <div class="card mb-3 mx-auto">
+    <div class="row g-0">
+      <div class="col-md-6">
+        <h1>世界で一番値段が高い車</h1>
+        <div class="container mb-6">
+          <div class="row gx-5">
+            <div class="col-sm">
+              <h2>メルセデスベンツ 300slr ウーレンハウトクーペ</h2>
+              <img src="https://preview.free3d.com/img/2018/11/2399374330313049397/jafz01by.jpg"
+                class="img-fluid rounded-start" alt="...">
             </div>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-6"><span class="badge bg-secondary">1</span></div>
+            <p>オークションで1億3500万ユーロ（約213億1000万円）という破格の値段で落札された</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-6"><span class="badge bg-secondary">2</span></div>
+            <p>"SLR"はドイツ語でSport, Leicht, Rennwagen（スポーツ・軽量・レーシングカー）をあらわす</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <h1>日本で一番値段が高い車</h1>
+        <div class="container mb-6">
+          <div class="row gx-5">
+            <div class="col-sm">
+              <h2>レクサスLFA</h2>
+              <img src="https://img.amiami.jp/images/product/main/231/TOY-SCL3-53269.jpg"
+                class="img-fluid rounded-start" alt="...">
+            </div>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+            <p>価格は3750万円（ニュルブルクリンクパッケージは約4500万円）</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+            <p>最高速度は320km/h</p>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="container mb-5">
-    <h2>作り方</h2>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
-        <p>☆をボウルに混ぜ合わせておく。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
-        <p>別のボウルに★を全て混ぜ合わせておく。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">3</span></div>
-        <p>肉の厚みがある部分を切り開き、厚みを一定にする。<br />鶏肉１枚を２～３等分の大きめに切る。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">4</span></div>
-        <p>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">5</span></div>
-        <p>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。<br />
-          粉が余るようでしたら、二度付けをお勧めします！</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">6</span></div>
-        <p>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">7</span></div>
-        <p>油を160度に温める。<br />
-          (温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">8</span></div>
-        <p>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">9</span></div>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。
-      </div>
 
-    </div>
-    <ol>
-
-    </ol>
-  </div>
   <div class="container mb-5">
+    <h1>日本で一番売れた車（2023年）</h1>
     <div class="row gx-5">
       <div class="col-sm">
-        <h3>コツ・ポイント</h3>
-
-        <p>揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</p>
-        <p>好みで粉類に粗びき黒胡椒小さじ1/2を足しても◎</p>
-        <p>にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</p>
-        <p>パプリカパウダーは無くても可</p>
+        <h3>登録車で１位はトヨタ「ヤリス」</h3>
+        <img src="https://www.weins-toyota-kanagawa.co.jp/file/cms/13701/3124628/image.jpg"
+          class="img-fluid rounded-start" alt="...">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+            <p>販売台数19万4364台</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+            <p>2020年から4年連続でトップ</p>
+          </div>
+        </div>
       </div>
+    </div>
+    <div class="row gx-5">
       <div class="col-sm">
-        <h3>このレシピの生い立ち</h3>
-
-        <p>今まで作ったKFCレシピは、卵液や粉もかなり余るし、何よりも
-          何かひと味足りない……と思って、やっとKFC味にたどり着きました！</p>
-        <p>どの家庭にもあるような材料で☆</p>
-        <p>牛乳と卵液、粉類も必要量のみになっているので無駄が無く経済的です☆</p>
+        <h3>軽四輪車で１位はホンダ「N-BOX」</h3>
+        <img src="https://tjniigata.jp/assets/uploads/2022/01/NBOX-760x503.jpg"
+          class="img-fluid rounded-start" alt="...">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">1</span></div>
+            <p>販売台数23万1385台</p>
+          </div>
+          <div class="col border p-2">
+            <div class="mb-2"><span class="badge bg-secondary">2</span></div>
+            <p>ホンダ「N-BOX」が唯一20万台超えの販売台数を記録して1位となった</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
- const data = useRecipe()
+const data = useRecipe()
 </script>
